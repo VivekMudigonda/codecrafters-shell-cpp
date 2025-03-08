afe1fa171528fa8a2d191781c91520da88424db6
vivekvardhan-god <vivekmudigonda@gmail.com>
2025-03-08 11:44:40 +0530

    Implement input parser and exit function

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -10,6 +13,21 @@ int main() {
     std::cout << "$ ";
 
     std::getline(std::cin, input);
+
+    std::stringstream s(input);
+
+    std::string inputPart;
+    char c = ' ';   
+    std::vector<std::string> inputSeg;
+
+    while(getline(s,inputPart,c)){
+      inputSeg.push_back(inputPart);
+    }
+
+    if(inputSeg[0]=="exit" && inputSeg[1]=="0"){
+      break;
+    }
+
     std::cout << input << ": command not found" << std::endl;
   }
 }
